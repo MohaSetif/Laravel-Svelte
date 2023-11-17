fa119bbf142940999b4a9956e917c8132d052436
MohaSetif <BouroubaMohamed004@gmail.com>
2023-11-17 15:06:43 +0100

    0.1

---
+++ resources/js/Pages/Home.svelte
@@ -0,0 +1,87 @@
+<script>
+    import Svelte from "../../img/svelte-icon.852x1024.png"
+    import Laravel from "../../img/laravel.995x1024.png"
+    import Inertia from "../../img/inertia.png"
+    import Tailwind from "../../img/tailwind.png"
+</script>
+
+<body class="antialiased">
+    <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+        <div class="max-w-9xl mx-auto p-6 lg:p-8">
+            <div>
+                <div class="grid grid-cols-1 md:grid-cols-2 gap-2 lg:gap-4">
+                    <a href="https://laravel.com/docs" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                        <div>
+                            <div class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
+                                <img src={Laravel} alt="Laravel" class="w-8 h-8">
+                            </div>
+
+                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Laravel</h2>
+
+                            <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                                Laravel is a popular PHP framework for building web applications. It provides a robust backend structure for your application, including routing, controllers, and database management.
+                            </p>
+                        </div>
+
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+                        </svg>
+                    </a>
+
+                    <a href="https://inertiajs.com/" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-purple-500">
+                        <div>
+                            <div class="h-16 w-16 bg-purple-50 dark:bg-purple-800/20 flex items-center justify-center rounded-full">
+                                <img src={Inertia} alt="Laravel" class="w-8 h-8">
+                            </div>
+
+                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Inertia.js</h2>
+
+                            <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                                Inertia.js is a library that allows you to build single-page applications (SPAs) using server-side routing and controllers in Laravel. It enables you to create dynamic, client-side experiences without building a traditional API. Instead, Inertia.js uses your Laravel routes and controllers to fetch and render views on the client side.
+                            </p>
+                        </div>
+
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="self-center shrink-0 stroke-purple-500 w-6 h-6 mx-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+                        </svg>
+                    </a>
+
+                    <a href="https://svelte.dev/docs/introduction" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-orange-500">
+                        <div>
+                            <div class="h-16 w-16 bg-orange-50 dark:bg-orange-800/20 flex items-center justify-center rounded-full">
+                                <img src={Svelte} alt="Laravel" class="w-8 h-8">
+                            </div>
+
+                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Svelte</h2>
+
+                            <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                                Svelte is a JavaScript framework for building user interfaces. It's different from traditional frameworks like React or Vue because it compiles components into highly efficient JavaScript at build time, resulting in smaller bundle sizes and faster runtime performance.
+                            </p>
+                        </div>
+
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="self-center shrink-0 stroke-orange-500 w-6 h-6 mx-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+                        </svg>
+                    </a>
+
+                    <a href="https://tailwindcss.com/docs/installation" class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-blue-500">
+                        <div>
+                            <div class="h-16 w-16 bg-blue-50 dark:bg-blue-800/20 flex items-center justify-center rounded-full">
+                                <img src={Tailwind} alt="Laravel" class="w-8 h-8">
+                            </div>
+
+                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Tailwind</h2>
+
+                            <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                                Tailwind CSS is a utility-first CSS framework. It provides a set of predefined utility classes that you can use to style your components without writing custom CSS. This approach promotes consistency and speeds up the development process.
+                            </p>
+                        </div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="self-center shrink-0 stroke-blue-500 w-6 h-6 mx-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
---
+++ tailwind.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./resources/**/*.svelte",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
